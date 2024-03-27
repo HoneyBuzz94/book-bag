53366d59184361f85e673e2ee677fda4d15203af
HoneyBuzz94 <144499231+HoneyBuzz94@users.noreply.github.com>
2024-03-26 19:38:15 -0600

    Refactored auth.js in server

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,3 +1,6 @@
+// REFACTORED
+
+const { GraphQLError } = require('graphql');
 const jwt = require('jsonwebtoken');
 
 // set token secret and expiration date
@@ -5,6 +8,11 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+  AuthenticationError: new GraphQLError('Could not authenticate user.', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
@@ -37,3 +45,5 @@ module.exports = {
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
+
+// REFACTORED
